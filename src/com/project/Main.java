1d89bf952dbf57cc779019df7989333febf3cfb8
geogardow <85602062+geogardow@users.noreply.github.com>
2021-09-03 11:53:37 -0600

    Creación clase Reto
    Se creó la clase reto, genera un reto aleatorio y se abre un box para recibir respuesta

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,10 +1,91 @@
 package com.project;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 public class Main {
 
     public static Interface example;
+    public static Reto reto;
     public static void main(String[] args) {
 	 example = new Interface();
+	 reto = new Reto();
+	 reto.start(example.pane);
+    }
+}
+class Reto extends JFrame implements ActionListener {
+    JButton space;
+    JPanel msgBox;
+    JLabel challenge;
+    JTextField answer;
+    JButton ask;
+    Random rand = new Random();
+    int a,b,op,res,x,y;
+    String reto="";
 
+    public void start(JPanel juego) {
+        op = rand.nextInt(5-1) + 1;
+        a = rand.nextInt(51-1) + 1;
+        b = rand.nextInt(51-1) + 1;
+        x=50;
+        y=50;
+        space = new JButton("Ok");
+        space.setSize(50,50);
+        space.setLocation(x,y);
+        juego.add(space);
+
+
+        setTitle("Reto");
+        setVisible(true);
+        setSize(300,200 );
+        msgBox = new JPanel();
+        this.getContentPane().add(msgBox);
+        msgBox.setLayout(null);
+        msgBox.setBackground(Color.decode("#bad5ff"));
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        if (op==1){
+            reto=a+"+"+b;
+        }
+        if (op==2){
+            reto=a+"-"+b;
+        }
+        if (op==3){
+            reto=a+"/"+b;
+        }
+        if (op==4){
+            reto=a+"*"+b;
+        }
+
+        challenge = new JLabel("¿Cuál es el valor de "+reto+"?");
+        challenge.setSize(200,20);
+        challenge.setLocation(60,30);
+        msgBox.add(challenge);
+
+        answer = new JTextField();
+        answer.setSize(50,20);
+        answer.setLocation(115,70);
+        msgBox.add(answer);
+
+        ask = new JButton("Ok");
+        ask.setSize(50,20);
+        ask.setLocation(115,110);
+        msgBox.add(ask);
+        ask.addActionListener(this::actionPerformed);
+        msgBox.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            res = Integer.parseInt(answer.getText());
+        } catch (NumberFormatException n) {
+            JOptionPane.showMessageDialog(null,"Sólo se permite la entrada de enteros");
+            answer.setText(null);
+        }
     }
 }
